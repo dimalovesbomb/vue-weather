37d0c745894c0e61753931ad9b96a93f6fe86da7
Dmitrii Kupriunin <kupriunin@gmail.com>
2020-11-12 16:00:43 +0300

    fixing material

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('./scss/index.scss');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
 import { MdAutocomplete } from 'vue-material/dist/components';
 
 Vue.config.productionTip = false;
